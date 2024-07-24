381a1528b70ab4ef50822992a2fa0a8094518146
Fatih Demir <fd1134@gmail.com>
2024-07-25 00:33:25 +0300

    static file update

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ const ejs=require("ejs");
 
 const app=express();
 
-//Middlware
-app.use(express.static("public"));
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
-// Template Engine
-app.set('view engine', 'ejs')
+//Middleware
+app.use(express.static('public'));
 
+//Routes
 app.get("/",(req,res)=>{
     
     res.render("index")
 
 });
+
+app.get("/about",(req,res)=>{
+
+    res.render("about")
+});
+
+app.get("/add",(req,res)=>{
+    res.render("add_post") //özelikle Böyle yaptım add deyince add_post.ejs render ettim 
+});
+
+
 
 const port=3000;
 app.listen(port,()=>{
